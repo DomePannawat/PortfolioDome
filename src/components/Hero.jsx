3354bb90100a7274e5c323c4bb47a69352c9bca8
Domepannawat <domepannawat@gmail.com>
2024-12-09 17:56:41 +0700

    PDF DOME

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             DomePannawat
           </h1>
           <a
-            href="/CV-DomePannawat.pdf"
+            href="/Cv-DomePannawat.pdf"
             target="_blank"
             className="text-nowrap rounded-lg border border-indigo-600 bg-black px-5 py-3 text-lg font-bold text-white shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600 mt-4"
           >
